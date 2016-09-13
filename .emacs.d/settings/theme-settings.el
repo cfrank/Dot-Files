8ba32042a492c4c33f629c4deb156f7563daf741
Chris Frank <chris@cfrank.org>
2016-09-13 13:47:12 +0300

    Add starting emacs settings

---
+++ .emacs.d/settings/theme-settings.el
@@ -0,0 +1,11 @@
+;; Add theme path
+(setq custom-theme-directory (concat user-emacs-directory "themes"))
+
+(dolist
+    (path (directory-files custom-theme-directory t "\\w+"))
+  (when (file-directory-p path)
+    (add-to-list 'custom-theme-load-path path)))
+
+(load-theme 'brin t)
+
+(provide 'theme-settings)
